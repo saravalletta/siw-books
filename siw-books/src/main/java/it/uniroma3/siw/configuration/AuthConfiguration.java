4b5bc1e9b678ca348fa0e139d34257be8758372f
Sara <sar.valletta@stud.uniroma3.it>
2025-06-05 23:26:17 +0200

    fix credenziali + inizio authconfiguration

---
+++ siw-books/src/main/java/it/uniroma3/siw/configuration/AuthConfiguration.java
@@ -0,0 +1,8 @@
+package it.uniroma3.siw.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AuthConfiguration {
+
+}
--- siw-books/src/main/java/it/uniroma3/siw/model/Credenziali.java
+++ siw-books/src/main/java/it/uniroma3/siw/model/Credenziali.java
@@ -1,6 +1,7 @@
 package it.uniroma3.siw.model;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,7 +18,9 @@ public class Credenziali {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	@Column(nullable=false, unique=true)
 	private String username;
+	@Column(nullable=false)
 	private String password;
 	private String ruolo;
 	@OneToOne(cascade=CascadeType.ALL)
