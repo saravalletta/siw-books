70829433494e40e2b0bdac5a31bff1d572ee783f
Sara <sar.valletta@stud.uniroma3.it>
2025-06-08 22:27:28 +0200

    UserDto

--- siw-books/src/main/java/it/uniroma3/siw/model/UserDto.java
+++ siw-books/src/main/java/it/uniroma3/siw/model/UserDto.java
@@ -1,5 +1,57 @@
 package it.uniroma3.siw.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class UserDto {
 
+	@NotBlank
+	private String name;
+	@NotBlank
+	private String surname;
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	private String username;
+	@NotBlank
+	private String password;
+	
+	
+	// Getter e setter
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
