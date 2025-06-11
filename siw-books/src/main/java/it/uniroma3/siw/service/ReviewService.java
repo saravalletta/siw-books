fad01b915e67dc0a54496fdb0beca0b4f36a3cc6
Sara <sar.valletta@stud.uniroma3.it>
2025-06-11 22:00:19 +0200

    fix vari

--- siw-books/src/main/java/it/uniroma3/siw/service/ReviewService.java
+++ siw-books/src/main/java/it/uniroma3/siw/service/ReviewService.java
@@ -1,10 +1,12 @@
 package it.uniroma3.siw.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import it.uniroma3.siw.model.Review;
 import it.uniroma3.siw.repository.ReviewRepository;
 
+@Service
 public class ReviewService {
 	
 	@Autowired
