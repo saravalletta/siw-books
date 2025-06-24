e1f8b7f8eb5aa8f78a6a0a62b157dd771f829cf5
Sara <sar.valletta@stud.uniroma3.it>
2025-06-24 23:00:35 +0200

    rimozione import.sql

--- siw-books/src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
--- INSERT INTO libro (titolo, annoPubblicazione, urlImmagine) VALUES ('Il nome della Rosa', 1980, '/immagine-homepage.png'); --
--- INSERT INTO libro (titolo, annoPubblicazione, urlImmagine) VALUES ('Il nome della Rosa', 1980, '/immagine-homepage.png'); --
--- INSERT INTO libro (titolo, annoPubblicazione, urlImmagine) VALUES ('Il nome della Rosa', 1980, '/immagine-homepage.png'); --
-
--- INSERT INTO autore (nome, cognome, dataDiNascita, dataDiMorte, nazionalità, urlImmagine) VALUES ('Umberto', 'Eco', '1932-01-05', '2016-02-19', 'Italia', '/immagine-homepage.png'); --
