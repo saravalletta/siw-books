a15429290acbb41c960c038463674d5968716564
Sara <sar.valletta@stud.uniroma3.it>
2025-07-22 22:03:11 +0200

    inizio query in authorRepository e bookRepository

--- siw-books/src/main/java/it/uniroma3/siw/repository/AuthorRepository.java
+++ siw-books/src/main/java/it/uniroma3/siw/repository/AuthorRepository.java
@@ -2,6 +2,7 @@ package it.uniroma3.siw.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import it.uniroma3.siw.model.Author;
@@ -9,5 +10,8 @@ import it.uniroma3.siw.model.Author;
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 	
 	public List<Author> findAll();
+	
+	@Query("select a from Autore a where concat(a.id,'',a.nome,'',a.cognome,'',a.nazionalita) LIKE %?1%")
+    public List<Author> findAllWithThatKeyWord(String keyWord);
 
 }
--- siw-books/src/main/java/it/uniroma3/siw/repository/BookRepository.java
+++ siw-books/src/main/java/it/uniroma3/siw/repository/BookRepository.java
@@ -1,7 +1,9 @@
 package it.uniroma3.siw.repository;
 
 import java.util.List;
+import java.util.Set;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import it.uniroma3.siw.model.Book;
@@ -9,5 +11,8 @@ import it.uniroma3.siw.model.Book;
 public interface BookRepository extends CrudRepository<Book, Long> {
 	
 	List<Book> findAll();
+	
+	@Query("select l from Libro l where CONCAT(l.id,'',l.titolo) LIKE %?1%")
+    public Set<Book> findAllWithThatKeyword(String keyword);
 
 }
